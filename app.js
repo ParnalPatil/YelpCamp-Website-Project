e1eea9697cc8e5eed72fcc0bb9a9fd6370673609
ParnalPatil <38610661+ParnalPatil@users.noreply.github.com>
2020-05-28 13:34:03 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+   	res.locals.success = req.flash("success");
+	next();
+});
+
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+
+app.listen(3000, function(){
+	console.log("Listening");
+});
